f7c1ed95de2a6d8017e4307a29417a2028a33f80
Koyyadirohithreddy <koyyadi358@gmail.com>
2025-04-02 16:10:26 -0500

    Delete project-galleryy-main directory

--- project-galleryy-main/tasks/t1.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("submitButton").addEventListener("click", function () {
-        let name = document.getElementById("textInput1").value;
-        alert("Hello from JavaScript");
-        alert(name);
-    });
-});
